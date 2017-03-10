9d3f84392a16c76eff57b1ed65ffb3b66171ab65
Wesley Vieira <wesleyvieira12@gmail.com>
2017-03-10 15:12:47 -0300

    alteraçao da migrate user

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20170310162015) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.         "kind"
+    t.integer  "kind"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
